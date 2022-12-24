e84dee2501d74b32895019646a8dd12f69a0d872
daria <daria@DESKTOP-7DKPVV8.localdomain>
2022-12-24 17:08:34 +0200

    Initial commit test

---
+++ src/hw1.java
@@ -0,0 +1,15 @@
+public class hw1 {
+    public static void main(String[] args) {
+        String name = "Daria ";
+        String surname = "Chervonooka";
+        String ns = name + surname;
+        System.out.printf(ns);
+
+
+        float a = 1.4F;
+        float b = 1.4F;
+        float c = a+b;
+        System.out.print("\n");
+        System.out.print(c);
+    }
+}
